3ddba4044a0e975daa1a5dda994b38a426e0564b
MasterTatu <44836865+MasterTatu@users.noreply.github.com>
2019-11-28 23:59:42 +0400

    Add files via upload

---
+++ autoinstall.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    system("pkg update && pkg upgrade && pkg install root-repo && termux-setup-storage && pkg install metasploit && pkg install git && cd && git clone https://github.com/ElevenPaths/Eternalblue-Doublepulsar-Metasploit && cd Eternalblue-Doublepulsar-Metasploit && cp eternalblue_doublepulsar.rb /data/data/com.termux/files/usr/opt/metasploit-framework/modules/exploits/smb && cd /data/data/com.termux/files/home && mkdir .wine && cd .wine && mkdir drive_c && msfconsole");
+    return 0;
+}
